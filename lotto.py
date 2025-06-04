ad58f3c12d3164558890777a760d520a2a94776b
Amelia1 <disputedbowl@gmail.com>
2025-06-04 10:48:18 +0000

    lotto

--- lotto.py
+++ lotto.py
@@ -1,9 +1,39 @@
 import random
+from colorama import Fore, Back, Style
 
-def new_roll():    
-    li = random.sample(range(1, 101), 5)
-    print(li)
+roll_num = 100000
 
-new_roll()
+def roll():
+    random.seed()    
+    return random.sample(range(1, 49), 6)
+    
+def simulate():
+    game = roll()
+    player = roll()
+    x = 0
+    for num in game:
+        for guess in player:
+            if num == guess:
+                x += 1
+    return x
+
+def simulation(amount):
+    print(Fore.RED)
+    print(f"\n-------------- Neues Spiel mit {roll_num} Versuchen ------------\n")
+    print(Fore.GREEN + "Simuliert....\n")
+    results = [0,0,0,0,0,0,0]
+    for i in range(amount):
+        results[simulate()] += 1
+    print(Fore.GREEN + "Ergebnisse: \n")
+    for i in range(len(results)):
+        prob = results[i] / roll_num * 100
+        print(f"{i} Richtig: {round(prob,2)}% | {results[i]} mal von {roll_num} Versuchen")
+    print(Fore.RED + "\n--------------------------------------------------------------\n")
+
+simulation(roll_num)
+
+
+
+
 
     
